765bd0b45ccfa3d5cf6a7c6f5a4bea86eef023db
fchzo08 <89438930+fchzo08@users.noreply.github.com>
2024-05-18 06:40:18 +0700

    Update server.js
    update json

--- server.js
+++ server.js
@@ -11,27 +11,27 @@ app.get('/',(req,res) =>{
     res.send('auth');
 })
 
-app.post('/a/getToken', (req, res) => {
+app.post('/api/getToken', (req, res) => {
     const { userId } = req.body;
     
     if (!userId) {
-      return res.status(400).send('User ID is required');
+      return res.status(400).json({ message: 'User ID is required' });
     }
   
     const token = jwt.sign({ userId }, SECRET_KEY, { expiresIn: '1h' });
     res.json({ token });
 });
 
-app.post('/a/verifyToken', (req, res) => {
+app.post('/api/verifyToken', (req, res) => {
     const token = req.body.token;
     if (!token) {
-      return res.status(401).send('Access denied. Token is required');
+      return res.status(401).json({ message: 'Access denied. Token is required' });
     }
   
     jwt.verify(token, SECRET_KEY, (err, decoded) => {
       if (err) {
         console.error('Token verification error:', err);
-        return res.status(403).send('Access denied. Invalid token');
+        return res.status(403).json({ message: 'Access denied. Invalid token'});
       }
       res.json({ userId: decoded.userId });
     });
